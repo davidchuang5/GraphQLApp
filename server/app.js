4b87946f971fdd40da7b38c052c2171ec91067d1
davidchuang5 <davidchuang5@gmail.com>
2023-11-28 18:46:02 -0600

    Added Apollo Server

--- server/app.js
+++ server/app.js
@@ -2,16 +2,39 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
+const { ApolloServer } = require('apollo-server');
 const bookSchema = require('./models/book');
 const authorSchema = require('./models/author');
 const cors = require('cors');
 
-mongoose.connect('mongodb+srv://davidchuang5:Andyir0ns@codesmithgradtest.akyjqlo.mongodb.net/');
-mongoose.connection.once('open', () => {
-  console.log('Connected to database');
-});
+// Apollo Server requires:
+// 1. typeDefs: GraphQL type definitions
+// 2. resolvers: How do we resolve queries / mutations
+
+const MONGODB = 'mongodb+srv://davidchuang5:Andyir0ns@codesmithgradtest.akyjqlo.mongodb.net/';
+// mongoose.connect('mongodb+srv://davidchuang5:Andyir0ns@codesmithgradtest.akyjqlo.mongodb.net/');
+// mongoose.connection.once('open', () => {
+//   console.log('Connected to database');
+// });
+
+// Connect to mongodb first becasue Apollo server relies on this connection
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connection Successful');
+    return server.listen({ port: 4000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
 
 const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 app.use(cors());
 
 app.use(
